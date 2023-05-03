af7f217979ada4259ad9259a20e71934c37bb641
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-05-03 19:57:44 +0300

    trying to work with sass

--- project-no-2/src/App.jsx
+++ project-no-2/src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import './App.css'
+import './scss/style.css'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 
--- project-no-2/src/components/Navbar.jsx
+++ project-no-2/src/components/Navbar.jsx
@@ -1,5 +1,10 @@
+// import 
 function Navbar() {
-    return <h1>Navbar goes here</h1>
+  return (
+    <div className="navbar">
+      <h1>Navbar goes here</h1>
+    </div>
+  );
 }
 
 export default Navbar;
